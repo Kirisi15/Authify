9033f8bc24a60289ad46eefac8970bcbb0fcfd00
Kirisi15 <kkirisigan@gmail.com>
2025-05-20 14:21:23 +0530

    Restrict duplicate entry using email

--- Authify/src/main/java/com/example/Authify/Service/ProfileServiceImpl.java
+++ Authify/src/main/java/com/example/Authify/Service/ProfileServiceImpl.java
@@ -4,7 +4,9 @@ import com.example.Authify.Repository.UserRepository;
 import com.example.Authify.entiry.UserEntity;
 import com.example.Authify.io.ProfileRequest;
 import com.example.Authify.io.ProfileResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -21,8 +23,12 @@ public class ProfileServiceImpl implements ProfileService {
     @Override
     public ProfileResponse createProfile(ProfileRequest request) {
         UserEntity newProfile = convertToUserEntity(request);
-        newProfile = userRepository.save(newProfile);
-        return convertToProfileResponse(newProfile);
+        if(!userRepository.existsByEmail(request.getEmail())) {
+            newProfile = userRepository.save(newProfile);
+            return convertToProfileResponse(newProfile);
+        }
+        throw new ResponseStatusException(HttpStatus.CONFLICT,"Email already exists");
+
     }
 
     private ProfileResponse convertToProfileResponse(UserEntity newProfile) {
